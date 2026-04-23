4ffda3a1cc472db9eb108fbe3b61ec0a979b090b
harcastic <harsh.raj2274@gmail.com>
2026-04-23 18:02:51 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,6 @@ app.delete('/todos/:id', (req, res) => {
   res.json(deletedTodo[0]);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
